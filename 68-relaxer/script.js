f94e744942e79ab3d37d73c7c224d13190b62c68
Ahmet Suhan OKA <ahmetsuhanoka0@gmail.com>
2021-11-05 09:51:17 +0300

    add relaxer

---
+++ 68-relaxer/script.js
@@ -0,0 +1,31 @@
+const App = (function () {
+  const container = document.getElementById("container");
+  const text = document.getElementById("text");
+
+  const totalTime = 7500;
+  const breathTime = (totalTime / 5) * 2;
+  const holdTime = totalTime / 5;
+
+  function breathAnimation() {
+    text.innerText = "Breathe In!";
+    container.className = "container grow";
+    setTimeout(() => {
+      text.innerText = "Hold";
+      setTimeout(() => {
+        text.innerText = "Breathe Out!";
+        container.className = "container shrink";
+      }, holdTime);
+    }, breathTime);
+  }
+
+  function runBreathAnimation() {
+    setInterval(breathAnimation, totalTime);
+  }
+
+  return {
+    start: function () {
+      runBreathAnimation();
+    },
+  };
+})();
+window.addEventListener("DOMContentLoaded", App.start());
